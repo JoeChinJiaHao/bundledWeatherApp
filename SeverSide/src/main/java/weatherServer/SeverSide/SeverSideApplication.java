5fb5539aa3181cbaad1fa367629fb7e23a2a36f8
Joe Chin <joe.chin.jia.hao@gmail.com>
2022-02-08 12:35:33 +0800

    seversideupdate

---
+++ SeverSide/src/main/java/weatherServer/SeverSide/SeverSideApplication.java
@@ -0,0 +1,13 @@
+package weatherServer.SeverSide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeverSideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeverSideApplication.class, args);
+	}
+
+}
